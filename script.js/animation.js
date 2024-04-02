f6309912e489491e44e751f81cefe15d057778ae
Maxim Zavidiy <zavidiy1998@mail.ru>
2024-04-02 18:53:25 +0200

    v2

--- script.js/animation.js
+++ script.js/animation.js
@@ -2,9 +2,9 @@ function animation(){
     const blockTwo = document.querySelector('.block-two');
     const header = document.querySelector('.header');
     const pruning = document.querySelector('.pruning');
-    
+    const scrollClick = document.querySelectorAll('.scroll');
     let scrolltop = scrollY; // запомнить
-    window.addEventListener('scroll', function(){
+    window.addEventListener('scroll', function scroll(){
       if (scrollY >= scrolltop) { // сравнить
         pruning.classList.add('active');
         pruning.setAttribute('loop', 'true'); // Устанавливаем loop в true
@@ -12,17 +12,36 @@ function animation(){
         header.classList.add('removeBlock');
         setTimeout(() => {
             blockTwo.classList.add('active');
-        //    header.classList.add('removeBlock');
-        }, 500);
+        }, 400);
       } else {
         pruning.classList.remove('active');
-        pruning.removeAttribute('loop'); // Устанавливаем loop в false
-        pruning.pause(); // Останавливаем воспроизведение видео
+        pruning.removeAttribute('loop'); // Удаляем loop 
         blockTwo.classList.remove('active');
         header.classList.remove('removeBlock');
       }
       scrolltop = scrollY; // запомнить для последующих срабатываний
     });
+
+
+    scrollClick.forEach(click =>{
+        click.addEventListener('click', ()=>{
+            if (!header.classList.contains('removeBlock')) { // сравнить
+                pruning.classList.add('active');
+                pruning.setAttribute('loop', 'true'); // Устанавливаем loop в true
+                pruning.play(); // Начинаем воспроизведение видео
+                header.classList.add('removeBlock');
+                setTimeout(() => {
+                    blockTwo.classList.add('active');
+                }, 400);
+              } else {
+                pruning.classList.remove('active');
+                pruning.removeAttribute('loop'); // Удаляем loop 
+                blockTwo.classList.remove('active');
+                header.classList.remove('removeBlock');
+              }
+        })
+    })
 }
 
 animation();
+
