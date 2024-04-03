beb0f14ae87a41339fc41f3c2d757e6b46618df5
Maxim Zavidiy <zavidiy1998@mail.ru>
2024-04-03 13:42:04 +0200

    add reverse

--- script.js/animation.js
+++ script.js/animation.js
@@ -1,44 +1,80 @@
 function animation(){
-    const blockTwo = document.querySelector('.block-two');
-    const header = document.querySelector('.header');
-    const pruning = document.querySelector('.pruning');
-    const scrollClick = document.querySelectorAll('.scroll');
-    let scrolltop = scrollY; // запомнить
-    window.addEventListener('scroll', function scroll(){
-      if (scrollY >= scrolltop) { // сравнить
+  const blockTwo = document.querySelector('.block-two');
+  const header = document.querySelector('.header');
+  const pruning = document.querySelector('.pruning');
+  const scrollClick = document.querySelectorAll('.scroll');
+  const pruningReverse = document.querySelector('.pruning-reverse');
+  
+  let scrolltop = scrollY; // запомнить
+  
+  window.addEventListener('scroll', function scroll() {
+    if (scrollY >= scrolltop) {
         pruning.classList.add('active');
-        pruning.setAttribute('loop', 'true'); // Устанавливаем loop в true
-        pruning.play(); // Начинаем воспроизведение видео
+        pruning.setAttribute('loop', 'true');
+        pruning.play();
+        setTimeout(() => {
+          pruning.classList.remove('active');
+          pruning.removeAttribute('loop');
+        }, 800); 
+
         header.classList.add('removeBlock');
         setTimeout(() => {
             blockTwo.classList.add('active');
         }, 400);
-      } else {
-        pruning.classList.remove('active');
-        pruning.removeAttribute('loop'); // Удаляем loop 
+    } else {
+
+        pruningReverse.classList.add('active-reverse');
+        pruningReverse.setAttribute('loop', 'true');
+        pruningReverse.play();
+        setTimeout(() => {
+          pruningReverse.removeAttribute('loop');
+          pruningReverse.classList.remove('active-reverse');
+        }, 800); 
+
+        setTimeout(() => {
+            header.classList.remove('removeBlock');
+        }, 400);
+
         blockTwo.classList.remove('active');
-        header.classList.remove('removeBlock');
-      }
-      scrolltop = scrollY; // запомнить для последующих срабатываний
-    });
+    }
+    
+    scrolltop = scrollY;
+});
+
+
 
 
     scrollClick.forEach(click =>{
         click.addEventListener('click', ()=>{
             if (!header.classList.contains('removeBlock')) { // сравнить
-                pruning.classList.add('active');
-                pruning.setAttribute('loop', 'true'); // Устанавливаем loop в true
-                pruning.play(); // Начинаем воспроизведение видео
-                header.classList.add('removeBlock');
-                setTimeout(() => {
-                    blockTwo.classList.add('active');
-                }, 400);
-              } else {
+              pruning.classList.add('active');
+              pruning.setAttribute('loop', 'true');
+              pruning.play();
+              setTimeout(() => {
                 pruning.classList.remove('active');
-                pruning.removeAttribute('loop'); // Удаляем loop 
-                blockTwo.classList.remove('active');
-                header.classList.remove('removeBlock');
-              }
+                pruning.removeAttribute('loop');
+              }, 850); 
+      
+              header.classList.add('removeBlock');
+              setTimeout(() => {
+                  blockTwo.classList.add('active');
+              }, 350);
+          } else {
+      
+              pruningReverse.classList.add('active-reverse');
+              pruningReverse.setAttribute('loop', 'true');
+              pruningReverse.play();
+              setTimeout(() => {
+                pruningReverse.removeAttribute('loop');
+                pruningReverse.classList.remove('active-reverse');
+              }, 850); 
+      
+              setTimeout(() => {
+                  header.classList.remove('removeBlock');
+              }, 350);
+      
+              blockTwo.classList.remove('active');
+            }
         })
     })
 }
