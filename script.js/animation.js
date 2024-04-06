dee946081cf8823fc689e2efc9028019952ed593
Maxim Zavidiy <zavidiy1998@mail.ru>
2024-04-06 18:52:26 +0200

    rus/eng

--- script.js/animation.js
+++ script.js/animation.js
@@ -2,24 +2,46 @@ function animation(){
   const blockTwo = document.querySelector('.block-two');
   const header = document.querySelector('.header');
   const pruning = document.querySelector('.pruning');
-  const scrollClick = document.querySelectorAll('.scroll');
-  const pruningReverse = document.querySelector('.pruning-reverse');
+  const scrollClick = document.querySelector('.scroll');
+  const pruningReverse = document.querySelector('.pruningOne-reverse');
   const pruningReverse2 = document.querySelector('.pruningTwo-reverse');
   const blockThree = document.querySelector('.block-three');
   const pruning2 = document.querySelector('.pruning2');
+  const blockFour = document.querySelector('.block-four');
+  const pruning3 = document.querySelector('.pruning3');
+  const pruningReverse3 = document.querySelector('.pruningThree-reverse');
+  const footer = document.querySelector('footer');
 
   let lastScrollTop = 0; // Запомнить последнее положение прокрутки
   // Пороги прокрутки для блоков
- // const blockTwoThreshold = 99; // Примерное значение, замените на реальное
- // const blockThreeThreshold = 299; // Примерное значение, замените на реальное
-
-  window.addEventListener('scroll', function() {
-    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  const blockTwoThreshold = 100; // Примерное значение, замените на реальное
+  const blockThreeThreshold = 200; // Примерное значение, замените на реальное
+  const blockFourThreshold = 300; // Примерное значение, замените на реальное
+  const blockFooterThreshold = 400; // Примерное значение, замените на реальное
+  window.addEventListener('scroll', function(event) {
+    let scrollTop = window.scrollY || document.documentElement.scrollTop;
     let direction = scrollTop > lastScrollTop ? 'down' : 'up';
+    console.log(scrollTop)
 
     if (direction === 'down') {
-      //document.body.classList.add('body-no-scroll');
-        if (!blockThree.classList.contains('active') && blockTwo.classList.contains('active')) {
+  
+      event.stopPropagation();
+        if (!blockTwo.classList.contains('active') && blockTwoThreshold >= scrollTop) {
+           document.body.classList.add('body-no-scroll');
+           pruning.classList.add('active');
+           pruning.setAttribute('loop', 'true');
+           pruning.play();
+           setTimeout(() => {
+             pruning.classList.remove('active');
+             pruning.removeAttribute('loop');
+             document.body.classList.remove('body-no-scroll');
+           }, 800); 
+           header.classList.add('removeBlock'); 
+           setTimeout(() => {
+               blockTwo.classList.add('active');
+           }, 400);
+           blockThree.classList.remove('active');
+        } else if (!blockThree.classList.contains('active') && blockThreeThreshold >= scrollTop){
             // Показать blockThree и скрыть blockTwo
             document.body.classList.add('body-no-scroll');
             pruning2.classList.add('active');
@@ -33,43 +55,56 @@ function animation(){
             setTimeout(() => {
               blockThree.classList.add('active');
               blockTwo.classList.remove('active');
-            }, 400);
-        } else if (!blockTwo.classList.contains('active')){
-            // Показать blockTwo и скрыть blockThree
+            }, 500);
+          
+        } else if(!blockFour.classList.contains('active') && blockFourThreshold >= scrollTop){
+            // Показать blockFour и скрыть blockThree
             document.body.classList.add('body-no-scroll');
-            pruning.classList.add('active');
-            pruning.setAttribute('loop', 'true');
-            pruning.play();
+            pruning3.classList.add('active');
+            pruning3.setAttribute('loop', 'true');
+            pruning3.play();
             setTimeout(() => {
-              pruning.classList.remove('active');
-              pruning.removeAttribute('loop');
+              pruning3.classList.remove('active');
+              pruning3.removeAttribute('loop');
               document.body.classList.remove('body-no-scroll');
-            }, 800); 
-            header.classList.add('removeBlock'); 
+            }, 2000); 
             setTimeout(() => {
-                blockTwo.classList.add('active');
-            }, 400);
-            blockThree.classList.remove('active');
+              blockFour.classList.add('active');
+              blockThree.classList.remove('active');
+            }, 2000);
+
+            lastScrollTop = 300;
+
+        } else if(!footer.classList.contains('footerActive') && blockFooterThreshold >= scrollTop){
+            // Показать footer
+            document.body.classList.add('body-no-scroll');
+            setTimeout(() => {
+              document.body.classList.remove('body-no-scroll');
+              blockFour.classList.remove('active');
+              footer.classList.add('footerActive');
+            }, 300); 
+            scrollClick.style.opacity = '0';
         }
     } else {
       if (blockTwo.classList.contains('active')) {
-          // Показать blockTwo и скрыть blockThree
+        
           document.body.classList.add('body-no-scroll');
+          pruningReverse.classList.add('activeOneReverse');
           pruningReverse.setAttribute('loop', 'true');
           pruningReverse.play();
           setTimeout(() => {
             pruningReverse.removeAttribute('loop');
+            pruningReverse.classList.remove('activeOneReverse');
             document.body.classList.remove('body-no-scroll');
           }, 800); 
-        //  blockTwo.classList.remove('active');
-        //  setTimeout(() => {
-        //      header.classList.remove('removeBlock'); 
-        //  }, 400);
-         // blockThree.classList.remove('active');
-      }else if(blockThree.classList.contains('active')){
-
-        document.body.classList.add('body-no-scroll');
+          blockTwo.classList.remove('active');
+          setTimeout(() => {
+              header.classList.remove('removeBlock'); 
+          }, 400);
+        blockThree.classList.remove('active');
         
+      } else if (blockThree.classList.contains('active')) {
+        document.body.classList.add('body-no-scroll');
         pruningReverse2.classList.add('activeReverse');
         pruningReverse2.setAttribute('loop', 'true');
         pruningReverse2.play();
@@ -78,86 +113,93 @@ function animation(){
           pruningReverse2.classList.remove('activeReverse');
           document.body.classList.remove('body-no-scroll');
         }, 800); 
-      //  header.classList.add('removeBlock'); 
-      blockThree.classList.remove('active');
+        blockThree.classList.remove('active');
         setTimeout(() => {
           blockTwo.classList.add('active');
-        }, 400);
+        }, 500);
+      } else if (blockFour.classList.contains('active')){
+        document.body.classList.add('body-no-scroll');
+        pruningReverse3.classList.add('activeThreeReverse');
+        pruningReverse3.setAttribute('loop', 'true');
+        pruningReverse3.play();
+        setTimeout(() => {
+          pruningReverse3.removeAttribute('loop');
+          pruningReverse3.classList.remove('activeThreeReverse');
+          document.body.classList.remove('body-no-scroll');
+        }, 2000); 
+        //  header.classList.add('removeBlock'); 
+        blockFour.classList.remove('active');
+        setTimeout(() => {
+          blockThree.classList.add('active');
+        }, 2000);
+      } else if(footer.classList.contains('footerActive')){
+        // Скрыть footer
+        document.body.classList.add('body-no-scroll');
+        footer.classList.remove('footerActive');
+        setTimeout(() => {
+          document.body.classList.remove('body-no-scroll');
+          blockFour.classList.add('active');
+        }, 400); 
+        
+        scrollClick.style.opacity = '1';
       }
     } 
-    
+
     lastScrollTop = scrollTop;
 });
 
- /* window.addEventListener('scroll', function scroll() {
-    console.log(scrolltop)
-    if (header.style.opacity == 0 && scrolltop <= scrollY) {
-        document.body.classList.add('body-no-scroll');
-        pruning.classList.add('active');
-        pruning.setAttribute('loop', 'true');
-        pruning.play();
-        setTimeout(() => {
-          pruning.classList.remove('active');
-          pruning.removeAttribute('loop');
-          document.body.classList.remove('body-no-scroll');
-        }, 800); 
-        header.classList.add('removeBlock'); 
-        setTimeout(() => {
-            blockTwo.classList.add('active');
-        }, 400);
-    } else{
-        pruningReverse.classList.add('active-reverse');
-        pruningReverse.setAttribute('loop', 'true');
-        pruningReverse.play();
-        setTimeout(() => {
-          pruningReverse.removeAttribute('loop');
-          pruningReverse.classList.remove('active-reverse');
-        }, 800); 
-        setTimeout(() => {
-            header.classList.remove('removeBlock');
-        }, 400);
-        blockTwo.classList.remove('active');
-    }
 
-    scrolltop = scrollY;
-});*/
-
-
-
-
-    scrollClick.forEach(click =>{
-        click.addEventListener('click', ()=>{
-            if (!header.classList.contains('removeBlock')) { // сравнить
-              pruning.classList.add('active');
-              pruning.setAttribute('loop', 'true');
-              pruning.play();
-              setTimeout(() => {
-                pruning.classList.remove('active');
-                pruning.removeAttribute('loop');
-              }, 850); 
-      
-              header.classList.add('removeBlock');
-              setTimeout(() => {
-                  blockTwo.classList.add('active');
-              }, 350);
-          } else {
-      
-              pruningReverse.classList.add('active-reverse');
-              pruningReverse.setAttribute('loop', 'true');
-              pruningReverse.play();
-              setTimeout(() => {
-                pruningReverse.removeAttribute('loop');
-                pruningReverse.classList.remove('active-reverse');
-              }, 850); 
-      
-              setTimeout(() => {
-                  header.classList.remove('removeBlock');
-              }, 350);
-      
-              blockTwo.classList.remove('active');
-            }
-        })
-    })
+      scrollClick.addEventListener('click', (event)=>{
+          if (!blockTwo.classList.contains('active') && !header.classList.contains('removeBlock')) {
+            pruning.classList.add('active');
+            pruning.setAttribute('loop', 'true');
+            pruning.play();
+            setTimeout(() => {
+              pruning.classList.remove('active');
+              pruning.removeAttribute('loop');
+            }, 800); 
+            header.classList.add('removeBlock'); 
+            setTimeout(() => {
+                blockTwo.classList.add('active');
+            }, 400);
+            blockThree.classList.remove('active');
+         } else if (!blockThree.classList.contains('active')){
+          // Показать blockThree и скрыть blockTwo
+          pruning2.classList.add('active');
+          pruning2.setAttribute('loop', 'true');
+          pruning2.play();
+          setTimeout(() => {
+            pruning2.classList.remove('active');
+            pruning2.removeAttribute('loop');
+          }, 800); 
+          setTimeout(() => {
+            blockThree.classList.add('active');
+            blockTwo.classList.remove('active');
+          }, 500);
+         
+        } else if(!blockFour.classList.contains('active')){
+          // Показать blockFour и скрыть blockThree
+          pruning3.classList.add('active');
+          pruning3.setAttribute('loop', 'true');
+          pruning3.play();
+          setTimeout(() => {
+            pruning3.classList.remove('active');
+            pruning3.removeAttribute('loop');
+          }, 2000); 
+          setTimeout(() => {
+            blockFour.classList.add('active');
+            blockThree.classList.remove('active');
+          }, 2000);
+        } else if(!footer.classList.contains('footerActive')){
+          // Показать footer
+          setTimeout(() => {
+            footer.classList.add('footerActive');
+          }, 300); 
+          blockFour.classList.remove('active');
+          scrollClick.style.opacity = '0';
+        }
+        event.preventDefault();
+      })
 }
 
 animation();
--- script.js/show.js
+++ script.js/show.js
@@ -1,23 +1,38 @@
 /* Для всплывающего окна */ 
-let menuBtn = document.querySelectorAll('.menuBtn');
-let menus = document.querySelectorAll('nav');
+let menuBtn = document.querySelector('.menuBtn');
+let menu = document.querySelector('nav');
 
-menuBtn.forEach(elem => {
-    elem.addEventListener('click', ()=>{
-        elem.classList.toggle('active');
-        for(let menu of menus){
-            menu.classList.toggle('popUp');
+menuBtn.addEventListener('click', ()=>{
+    menuBtn.classList.toggle('active');
+    menu.classList.toggle('popUp');
 
-            document.addEventListener('click', function(event) {
-                // Проверяем, что клик произошел вне меню и кнопки открытия
-                if (!menu.contains(event.target) && !elem.contains(event.target)) {
-                    // Закрываем меню, добавляя класс, который изменяет его видимость или другие свойства
-                    menu.classList.remove('popUp');
-                    elem.classList.remove('active');
-                }
-            });
+    document.addEventListener('click', function(event) {
+        // Проверяем, что клик произошел вне меню и кнопки открытия
+        if (!menu.contains(event.target) && !menuBtn.contains(event.target)) {
+            // Закрываем меню, добавляя класс, который изменяет его видимость или другие свойства
+            menu.classList.remove('popUp');
+            menuBtn.classList.remove('active');
         }
-    })
-
+    });
 })
 
+function language(){
+    let languageBtn = document.querySelector('.language');
+    let engs = document.querySelectorAll('#eng');
+    let russ = document.querySelectorAll('#rus');
+    languageBtn.addEventListener('click', ()=>{
+        for(let eng of engs){
+            eng.classList.toggle('showEng');
+            if(eng.classList.contains('showEng')){
+                languageBtn.textContent = 'Rus';
+            }else{
+                languageBtn.textContent = 'Eng';
+            }
+        }
+        for(let rus of russ){
+            rus.classList.toggle('showRus');
+        }
+    })
+}
+
+language();
