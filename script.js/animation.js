4038f14afa6d5da59062165b37a4d0b61fc1c476
Maxim Zavidiy <zavidiy1998@mail.ru>
2024-04-10 19:16:24 +0200

    v2

--- script.js/animation.js
+++ script.js/animation.js
@@ -20,7 +20,6 @@
     document.body.appendChild(img);
     img.style.display = 'none'; // Скрываем изображение
    }
-   
    // Использование
    preloadImage('/img/3_robots.png');
 
@@ -43,48 +42,57 @@ function scrollElement(event){
     if (!ticking) {
         window.requestAnimationFrame(() => {
           if (direction === 'down') {
-
             if (!header.classList.contains('removeBlock')) {
-               for(let scrollText of scrollTexts){
-                scrollText.style.opacity = '0';
+
+              header.classList.add('removeBlock')
+
+              if(header.classList.contains('removeBlock') ){
+                  for(let scrollText of scrollTexts){
+                    scrollText.style.opacity = '0';
+                  }
+                  pruning.classList.add('active');
+                  pruning.setAttribute('loop', 'true');
+                  pruning.play();
+                  setTimeout(() => {
+                    pruning.classList.remove('active');
+                    pruning.removeAttribute('loop');
+                    blockTwo.classList.add('active');
+                  }, 1200); 
               }
-               pruning.classList.add('active');
-               pruning.setAttribute('loop', 'true');
-               pruning.play();
-               setTimeout(() => {
-                 pruning.classList.remove('active');
-                 pruning.removeAttribute('loop');
-                 blockTwo.classList.add('active');
-               }, 1200); 
-               header.classList.add('removeBlock'); 
+
     
             } else if (blockTwo.classList.contains('active')){
-                // Показать blockThree и скрыть blockTwo
-    
-                pruning2.classList.add('active');
-                pruning2.setAttribute('loop', 'true');
-                pruning2.play();
-                setTimeout(() => {
-                  pruning2.classList.remove('active');
-                  pruning2.removeAttribute('loop');
-         
-                  blockThree.classList.add('active');
-                }, 2000); 
+
                 blockTwo.classList.remove('active');
-      
+                if(!blockTwo.classList.contains('active')){
+                    // Показать blockThree и скрыть blockTwo
+                    pruning2.classList.add('active');
+                    pruning2.setAttribute('loop', 'true');
+                    pruning2.play();
+                    setTimeout(() => {
+                      pruning2.classList.remove('active');
+                      pruning2.removeAttribute('loop');
+            
+                      blockThree.classList.add('active');
+                    }, 2000); 
+                }
+
             } else if(blockThree.classList.contains('active')){
-                // Показать blockFour и скрыть blockThree
-        
-                pruning3.classList.add('active');
-                pruning3.setAttribute('loop', 'true');
-                pruning3.play();
-                setTimeout(() => {
-                  pruning3.classList.remove('active');
-                  pruning3.removeAttribute('loop');
-    
-                  blockFour.classList.add('active');
-                }, 2000); 
+
                 blockThree.classList.remove('active');
+                if(!blockThree.classList.contains('active')){
+                  // Показать blockFour и скрыть blockThree
+                  pruning3.classList.add('active');
+                  pruning3.setAttribute('loop', 'true');
+                  pruning3.play();
+                  setTimeout(() => {
+                    pruning3.classList.remove('active');
+                    pruning3.removeAttribute('loop');
+      
+                    blockFour.classList.add('active');
+                  }, 2000); 
+                } 
+          
             } else if(blockFour.classList.contains('active')){
                 // Показать footer
                 setTimeout(() => {
@@ -97,48 +105,54 @@ function scrollElement(event){
         } else if(direction === 'up'){
       
           if (blockTwo.classList.contains('active')) {
+            blockTwo.classList.remove('active');
               if(languageBtn.textContent == 'Eng'){
                 scrollTexts[0].style.opacity = '1';
               }else{
                 scrollTexts[1].style.opacity = '1';
               }
-            
-              pruningReverse.classList.add('activeOneReverse');
-              pruningReverse.setAttribute('loop', 'true');
-              pruningReverse.play();
-              setTimeout(() => {
-                pruningReverse.removeAttribute('loop');
-                pruningReverse.classList.remove('activeOneReverse');
-                header.classList.remove('removeBlock'); 
-    
-              }, 1200); 
-              blockTwo.classList.remove('active');
+
+              if(!blockTwo.classList.contains('active')){
+                pruningReverse.classList.add('activeOneReverse');
+                pruningReverse.setAttribute('loop', 'true');
+                pruningReverse.play();
+                setTimeout(() => {
+                  pruningReverse.removeAttribute('loop');
+                  pruningReverse.classList.remove('activeOneReverse');
+                  header.classList.remove('removeBlock'); 
+                }, 1200); 
+              }
     
             clickNum = 0;
           } else if (blockThree.classList.contains('active')) {
-            pruningReverse2.classList.add('activeReverse');
-            pruningReverse2.setAttribute('loop', 'true');
-            pruningReverse2.play();
-            setTimeout(() => {
-              pruningReverse2.removeAttribute('loop');
-              pruningReverse2.classList.remove('activeReverse');
-              blockTwo.classList.add('active');
-            }, 2000); 
+
             blockThree.classList.remove('active');
-    
+            if(!blockThree.classList.contains('active')){
+              pruningReverse2.classList.add('activeReverse');
+              pruningReverse2.setAttribute('loop', 'true');
+              pruningReverse2.play();
+              setTimeout(() => {
+                pruningReverse2.removeAttribute('loop');
+                pruningReverse2.classList.remove('activeReverse');
+                blockTwo.classList.add('active');
+              }, 2000); 
+            }
             clickNum = 1
+
           } else if (blockFour.classList.contains('active')){
-    
-            pruningReverse3.classList.add('activeThreeReverse');
-            pruningReverse3.setAttribute('loop', 'true');
-            pruningReverse3.play();
-            setTimeout(() => {
-              pruningReverse3.removeAttribute('loop');
-              pruningReverse3.classList.remove('activeThreeReverse');
-    
-              blockThree.classList.add('active');
-            }, 2000); 
+
             blockFour.classList.remove('active');
+            if(!blockFour.classList.contains('active')){
+              pruningReverse3.classList.add('activeThreeReverse');
+              pruningReverse3.setAttribute('loop', 'true');
+              pruningReverse3.play();
+              setTimeout(() => {
+                pruningReverse3.removeAttribute('loop');
+                pruningReverse3.classList.remove('activeThreeReverse');
+      
+                blockThree.classList.add('active');
+              }, 2000); 
+            }
     
             clickNum = 2;
           } else if(footer.classList.contains('footerActive')){
@@ -166,7 +180,11 @@ function scrollElement(event){
     //  requestAnimationFrame(updateScroll);
 }
 window.addEventListener('wheel', scrollElement, { passive: true });
-/*function updateScroll() {
+
+
+
+
+function updateScroll() {
   // Ваш код обновления состояния прокрутки
   console.log('1')
   // Проверяем, не прекратилась ли прокрутка
@@ -179,27 +197,7 @@ window.addEventListener('wheel', scrollElement, { passive: true });
 function stopScrolling() {
   isScrolling = false;
   console.log('2')
-}*/
-
-
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach(entry => {
-     if (entry.isIntersecting) {
-       // Элемент стал видимым, выполните необходимые действия
-       entry.target.classList.add('active');
-     } else {
-       // Элемент стал невидимым, выполните необходимые действия
-       entry.target.classList.remove('active');
-     }
-  });
- });
- 
- // Наблюдение за элементами
- observer.observe(document.querySelector('.block-two'));
- observer.observe(document.querySelector('.block-three'));
- observer.observe(document.querySelector('.block-four'));
-
-
+}
 
 function touchStart(event) {
   startY = event.touches[0].clientY;
@@ -210,35 +208,45 @@ const diffY = startY - currentY;
 
   if (diffY > 0) {
       if (!header.classList.contains('removeBlock')) {
-        for(let scrollText of scrollTexts){
-          scrollText.style.opacity = '0';
+
+        header.classList.add('removeBlock'); 
+        if(header.classList.contains('removeBlock')){
+          for(let scrollText of scrollTexts){
+            scrollText.style.opacity = '0';
+          }
+           pruning.classList.add('active');
+           pruning.setAttribute('loop', 'true');
+           pruning.play();
+           setTimeout(() => {
+             pruning.classList.remove('active');
+             pruning.removeAttribute('loop');
+      
+             blockTwo.classList.add('active');
+           }, 1200); 
         }
-         pruning.classList.add('active');
-         pruning.setAttribute('loop', 'true');
-         pruning.play();
-         setTimeout(() => {
-           pruning.classList.remove('active');
-           pruning.removeAttribute('loop');
-           blockTwo.classList.add('active');
-         }, 1200); 
-         header.classList.add('removeBlock'); 
          startY = currentY; // Обновляем startY после перехода
+
       } else if (blockTwo.classList.contains('active')){
-        // Показать blockThree и скрыть blockTwo
-
-        pruning2.classList.add('active');
-        pruning2.setAttribute('loop', 'true');
-        pruning2.play();
-        setTimeout(() => {
-          pruning2.classList.remove('active');
-          pruning2.removeAttribute('loop');
-
-          blockThree.classList.add('active');
-        }, 2000); 
+       
         blockTwo.classList.remove('active');
+        if(!blockTwo.classList.contains('active')){
+          // Показать blockThree и скрыть blockTwo
+          pruning2.classList.add('active');
+          pruning2.setAttribute('loop', 'true');
+          pruning2.play();
+          setTimeout(() => {
+            pruning2.classList.remove('active');
+            pruning2.removeAttribute('loop');
+          
+            blockThree.classList.add('active');
+          }, 2000); 
+        }
         startY = currentY; // Обновляем startY после перехода
+
       } else if(blockThree.classList.contains('active')){
 
+        blockThree.classList.remove('active');
+        if(!blockThree.classList.contains('active')){
           // Показать blockFour и скрыть blockThree
           pruning3.classList.add('active');
           pruning3.setAttribute('loop', 'true');
@@ -248,8 +256,9 @@ const diffY = startY - currentY;
             pruning3.removeAttribute('loop');
             blockFour.classList.add('active');
           }, 2000); 
-          blockThree.classList.remove('active');
-          startY = currentY; // Обновляем startY после перехода
+        }
+        startY = currentY; // Обновляем startY после перехода
+
       } else if(blockFour.classList.contains('active')){
           // Показать footer
           setTimeout(() => {
@@ -263,47 +272,58 @@ const diffY = startY - currentY;
     
   } else if(diffY < 0){
     if (blockTwo.classList.contains('active')) {
-      if(languageBtn.textContent == 'Eng'){
-        scrollTexts[0].style.opacity = '1';
-      }else{
-        scrollTexts[1].style.opacity = '1';
+
+      blockTwo.classList.remove('active');
+      if(!blockTwo.classList.contains('active')){
+        if(languageBtn.textContent == 'Eng'){
+          scrollTexts[0].style.opacity = '1';
+        }else{
+          scrollTexts[1].style.opacity = '1';
+        }
+          pruningReverse.classList.add('activeOneReverse');
+          pruningReverse.setAttribute('loop', 'true');
+          pruningReverse.play();
+          setTimeout(() => {
+            pruningReverse.removeAttribute('loop');
+            pruningReverse.classList.remove('activeOneReverse');
+            header.classList.remove('removeBlock'); 
+          }, 1200); 
       }
-        pruningReverse.classList.add('activeOneReverse');
-        pruningReverse.setAttribute('loop', 'true');
-        pruningReverse.play();
-        setTimeout(() => {
-          pruningReverse.removeAttribute('loop');
-          pruningReverse.classList.remove('activeOneReverse');
-          header.classList.remove('removeBlock'); 
-        }, 1200); 
-        blockTwo.classList.remove('active');
       clickNum = 0;
       startY = currentY; // Обновляем startY после перехода
+
     } else if (blockThree.classList.contains('active')) {
-      pruningReverse2.classList.add('activeReverse');
-      pruningReverse2.setAttribute('loop', 'true');
-      pruningReverse2.play();
-      setTimeout(() => {
-        pruningReverse2.removeAttribute('loop');
-        pruningReverse2.classList.remove('activeReverse');
-        blockTwo.classList.add('active');
-      }, 2000); 
+
       blockThree.classList.remove('active');
+      if(!blockThree.classList.contains('active')){
+        pruningReverse2.classList.add('activeReverse');
+        pruningReverse2.setAttribute('loop', 'true');
+        pruningReverse2.play();
+        setTimeout(() => {
+          pruningReverse2.removeAttribute('loop');
+          pruningReverse2.classList.remove('activeReverse');
+          blockTwo.classList.add('active');
+        }, 2000); 
+      }
       clickNum = 1
       startY = currentY; // Обновляем startY после перехода
+
     } else if (blockFour.classList.contains('active')){
 
-      pruningReverse3.classList.add('activeThreeReverse');
-      pruningReverse3.setAttribute('loop', 'true');
-      pruningReverse3.play();
-      setTimeout(() => {
-        pruningReverse3.removeAttribute('loop');
-        pruningReverse3.classList.remove('activeThreeReverse');
-        blockThree.classList.add('active');
-      }, 2000); 
       blockFour.classList.remove('active');
+      if(!blockFour.classList.contains('active')){
+        pruningReverse3.classList.add('activeThreeReverse');
+        pruningReverse3.setAttribute('loop', 'true');
+        pruningReverse3.play();
+        setTimeout(() => {
+          pruningReverse3.removeAttribute('loop');
+          pruningReverse3.classList.remove('activeThreeReverse');
+          blockThree.classList.add('active');
+        }, 2000); 
+      }
       clickNum = 2;
       startY = currentY; // Обновляем startY после перехода
+
     } else if(footer.classList.contains('footerActive')){
       // Скрыть footer
       setTimeout(() => {
@@ -319,14 +339,19 @@ const diffY = startY - currentY;
 
     
       // Если прокрутка уже идет, не делаем ничего
-    //  if (isScrolling) return;
+     if (isScrolling) return;
 
-      // Начинаем анимационный цикл
-    //  isScrolling = true;
-    //  requestAnimationFrame(updateScroll);
+    //   Начинаем анимационный цикл
+      isScrolling = true;
+      requestAnimationFrame(updateScroll);
 }
 window.addEventListener('touchstart', touchStart);
 window.addEventListener('touchmove', touchMoveScroll, { passive: true });
+window.addEventListener('touchmove', function() {
+  clearTimeout(scrollTimeout);
+  scrollTimeout = setTimeout(stopScrolling, 100); // Задержка 100 мс
+});
+
 
 
 let clickNum = 0;
@@ -349,46 +374,55 @@ function handleScrollClick() {
   });
 
   if (!blockTwo.classList.contains('active') && clickNum == 0) {
-    for(let scrollText of scrollTexts){
-      scrollText.style.opacity = '0';
+
+    header.classList.add('removeBlock'); 
+    if(header.classList.contains('removeBlock')){
+      for(let scrollText of scrollTexts){
+        scrollText.style.opacity = '0';
+      }
+       pruning.classList.add('active');
+       pruning.setAttribute('loop', 'true');
+       pruning.play();
+       setTimeout(() => {
+         pruning.classList.remove('active');
+         pruning.removeAttribute('loop');
+         blockTwo.classList.add('active');
+       }, 1200); 
     }
-     pruning.classList.add('active');
-     pruning.setAttribute('loop', 'true');
-     pruning.play();
-     setTimeout(() => {
-       pruning.classList.remove('active');
-       pruning.removeAttribute('loop');
-       blockTwo.classList.add('active');
-     }, 1200); 
-     header.classList.add('removeBlock'); 
+
   } else if (!blockThree.classList.contains('active') && clickNum == 1){
+
     // Показать blockThree и скрыть blockTwo
-
-    pruning2.classList.add('active');
-    pruning2.setAttribute('loop', 'true');
-    pruning2.play();
-    setTimeout(() => {
-      pruning2.classList.remove('active');
-      pruning2.removeAttribute('loop');
-
-      blockThree.classList.add('active');
-    }, 2000); 
     blockTwo.classList.remove('active');
-
+    if(!blockTwo.classList.contains('active')){
+      pruning2.classList.add('active');
+      pruning2.setAttribute('loop', 'true');
+      pruning2.play();
+      setTimeout(() => {
+        pruning2.classList.remove('active');
+        pruning2.removeAttribute('loop');
   
-  } else if(!blockFour.classList.contains('active') && clickNum == 2){
-    // Показать blockFour и скрыть blockThree
-    
-    pruning3.classList.add('active');
-    pruning3.setAttribute('loop', 'true');
-    pruning3.play();
-    setTimeout(() => {
-      pruning3.classList.remove('active');
-      pruning3.removeAttribute('loop');
+        blockThree.classList.add('active');
+      }, 2000); 
+    }
 
-      blockFour.classList.add('active');
-    }, 2000); 
+  } else if(!blockFour.classList.contains('active') && clickNum == 2){
+
+    // Показать blockFour и скрыть blockThree
     blockThree.classList.remove('active');
+    if(!blockThree.classList.contains('active')){
+      pruning3.classList.add('active');
+      pruning3.setAttribute('loop', 'true');
+      pruning3.play();
+      setTimeout(() => {
+        pruning3.classList.remove('active');
+        pruning3.removeAttribute('loop');
+  
+        blockFour.classList.add('active');
+      }, 2000); 
+    }
+
+
   } else if(!footer.classList.contains('footerActive')  && clickNum == 3){
     // Показать footer
     setTimeout(() => {
